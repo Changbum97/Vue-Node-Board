e867a2be559acb2987a33e93b60f62106073831c
Changbum97 <chb2005@naver.com>
2022-12-31 15:25:09 +0900

    FEAT: 이미지 업로드 기능 추가

--- node-back/models/content.js
+++ node-back/models/content.js
@@ -15,6 +15,10 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.INTEGER,
             allowNull: false,
         },
+        imagecnt: { // 이미지 개수
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        }
     },{
         charset: 'utf8',
         collate: 'utf8_general_ci',	// 이걸 해줌으로써 DB에 한글사용 가능
--- node-back/routes/content.js
+++ node-back/routes/content.js
@@ -2,6 +2,20 @@ var router = require('express').Router();
 var mysql = require('mysql2');
 var db = require('.././models');
 var Op = require('sequelize').Op;   // 검색 기능을 위해 사용
+var multer = require('multer');     // 이미지 업로드를 위해 사용
+var fs = require('fs');             // 이미지 업로드를 위해 사용
+
+// multer을 이용해 파일 업로드 기능 구현
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {	// 경로 => uploads 폴더
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {	// 파일명 => 이미지 업로드시 원본 이름 그대로
+        cb(null, file.originalname);
+    }
+})
+var upload = multer({ storage: storage });
+
 db.sequelize.sync();
 
 router.post('/write', function(req, res){		// 글 작성
@@ -10,6 +24,7 @@ router.post('/write', function(req, res){		// 글 작성
         writer: req.body.writer,
         title: req.body.title,
         text: req.body.text,
+        imagecnt: req.body.imagecnt
     }).then(function(){
         return res.status(200).json({
             message: '글 작성 완료!',
@@ -133,6 +148,33 @@ router.post('/search', function(req, res){
             return res.status(404).json({message: '에러뜸'});
         })
     }
+});
+
+// 글 작성 페이지에서 이미지를 올리면 실행되게 되는 부분
+router.post('/imagesave', upload.array('filelist'), function(req, res) {
+// 전송된 formdata의 filelist에 해당하는 value 값들을 multer을 통해 저장
+    var i, newname;
+    db.content.findOne({	// 새 글 작성시 기존에 있던 가장 큰 id+1로 자동 저장되므로 가장 큰 id를 찾아줌
+        limit: 1,
+        order: [['id', 'DESC']],
+        raw:true,
+    }).then(result => {
+        try {
+            newname = result.id;
+        } catch (e) {
+            // 등록된 글이 하나도 없으면 id가 Null이여서 에러 발생
+            // 이 상황에서는 newname을 0으로 설정해줌
+            newname = 0;
+        }
+        for(i=0;i<req.files.length;i++) {
+            fs.renameSync(req.files[i].path, 'uploads/'+(newname+1)+'-'+(i+1)+'.png');
+            // 위에서 이미지이름을 원본으로 저장해줬었는데 file system을 통해 이름을 바꿔주는 작업
+        }
+        return res.status(200).json({message:'이미지업로드완료!'});
+    }).catch(err => {
+        console.log(err);
+        return res.status(404).json({message: '에러뜸'});
+    })
 });
 
 module.exports = router;
